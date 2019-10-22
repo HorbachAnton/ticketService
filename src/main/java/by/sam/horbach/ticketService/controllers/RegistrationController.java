cca44ea2d8c6a0025d526d2e44fc5d78c9dd3816
OldManBlues <antngr@yandex.by>
2019-10-23 00:44:29 +0300

    Update.

--- src/main/java/by/sam/horbach/ticketService/controllers/RegistrationController.java
+++ src/main/java/by/sam/horbach/ticketService/controllers/RegistrationController.java
@@ -2,12 +2,12 @@ package by.sam.horbach.ticketService.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/registration")
 public class RegistrationController {
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public void registry() {
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public void register() {
 		
 	}
 }
