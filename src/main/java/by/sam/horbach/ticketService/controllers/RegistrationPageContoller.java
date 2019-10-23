36617c582085d7a353aa45dee13a8f20d086020d
OldManBlues <antngr@yandex.by>
2019-10-23 23:27:55 +0300

    Update.

---
+++ src/main/java/by/sam/horbach/ticketService/controllers/RegistrationPageContoller.java
@@ -0,0 +1,24 @@
+package by.sam.horbach.ticketService.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import by.sam.horbach.ticketService.entities.User;
+
+@Controller
+public class RegistrationPageContoller {
+
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public String getPage(@ModelAttribute("user")User user) {
+		return "registration";
+	}
+	
+	public void register(@ModelAttribute("user")User user, BindingResult result, ModelMap map) {
+		
+	}
+
+}
