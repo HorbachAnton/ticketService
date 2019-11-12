2661de0e9cd1e69eaadbf856c059dbc6c5804c1f
HorbachAnton <30475373+HorbachAnton@users.noreply.github.com>
2019-11-12 23:57:24 +0300

    Added new test data in ScriptDB.sql.

--- config/ScriptDB.sql
+++ config/ScriptDB.sql
@@ -129,12 +129,22 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 INSERT INTO `ticketservice`.`role` (`id`, `name`) VALUES ('1', 'CONSUMER');
 INSERT INTO `ticketservice`.`role` (`id`, `name`) VALUES ('2', 'ADMINISTRATOR');
+
 INSERT INTO `ticketservice`.`user` (`id`, `email`, `password`, `enabled`, `name`, `surname`, `Role_id`, `iconPath`) VALUES ('1', 'gleb@yandex.by', '$2y$12$boL9uxBRqPZWiBKVu.y8p.mxG8GMS8ytrNNOYKsN2RlgfNKIpO2CG', '1', 'Gleb', 'Fedorovich', '1', 'Не указано');
 INSERT INTO `ticketservice`.`user` (`id`, `email`, `password`, `enabled`, `name`, `surname`, `Role_id`, `iconPath`) VALUES ('2', 'dima@mail.ru', '$2a$10$198omg8vIWDCT7QOup61CO4LjXUg4cDkdNDK15uNLb9PrxDuCZTyK', '1', 'Не указано', 'Не указано', '2', 'Не указано');
+
 INSERT INTO `ticketservice`.`location` (`id`, `title`) VALUES ('1', 'Магнит');
 INSERT INTO `ticketservice`.`location` (`id`, `title`) VALUES ('2', 'ТЦ ПЛАЗА');
-INSERT INTO `ticketservice`.`event` (`id`, `title`, `summary`, `date`, `Location_id`, `price`, `iconPath`) VALUES ('1', 'Хеллоуин', 'Тест', '01.01.2019', '1', '245', 'Не указано');
+INSERT INTO `ticketservice`.`location` (`id`, `title`) VALUES ('3', 'Большая опера');
+INSERT INTO `ticketservice`.`location` (`id`, `title`) VALUES ('4', 'Кинотеатр');
+
+INSERT INTO `ticketservice`.`event` (`id`, `title`, `summary`, `date`, `Location_id`, `price`, `iconPath`) VALUES ('1', 'Хеллоуин', 'Тест', '2019-10-12 14:32:44', '1', '245', 'Не указано');
+INSERT INTO `ticketservice`.`event` (`id`, `title`, `summary`, `date`, `Location_id`, `price`, `iconPath`) VALUES ('2', 'Фестиваль', 'Тест', '2021-11-20 20:12:40', '2', '344', 'Не указано');
+INSERT INTO `ticketservice`.`event` (`id`, `title`, `summary`, `date`, `Location_id`, `price`, `iconPath`) VALUES ('3', 'Опера', 'Тест', '2018-12-14 17:34:40', '3', '421', 'Не указано');
+INSERT INTO `ticketservice`.`event` (`id`, `title`, `summary`, `date`, `Location_id`, `price`, `iconPath`) VALUES ('4', 'Фильм', 'Тест', '2020-08-03 15:28:17', '4', '297', 'Не указано');
+
 INSERT INTO `ticketservice`.`ticket` (`id`, `Event_id`, `User_id`) VALUES ('1', '1', '1');
+
 INSERT INTO `ticketservice`.`comment` (`id`, `User_id`, `Event_id`, `text`) VALUES ('1', '1', '1', 'Тест');
 
 
