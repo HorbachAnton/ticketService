c9d55ee348c4981d59b8c59aa03c5158b0ca2c2e
Horbach Anton <a.horbach@sam-solutions.com>
2019-11-13 11:51:13 +0300

    Added functionality of loading a list of upcoming events.

--- src/main/java/by/sam/horbach/ticketService/controllers/IndexPageController.java
+++ src/main/java/by/sam/horbach/ticketService/controllers/IndexPageController.java
@@ -5,11 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import by.sam.horbach.ticketService.facades.IndexFacade;
+
 @Controller
 public class IndexPageController {
 	
+	IndexFacade indexFacade;
+	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String getPage(Model model) {
+		model.addAttribute("upcomingEvents", indexFacade.getUpcomingEvents());
 		return "";
 	}
 
--- src/main/java/by/sam/horbach/ticketService/facades/IndexFacade.java
+++ src/main/java/by/sam/horbach/ticketService/facades/IndexFacade.java
@@ -2,16 +2,30 @@ package by.sam.horbach.ticketService.facades;
 
 import java.util.List;
 
+import org.springframework.binding.convert.converters.TwoWayConverter;
+
+import by.sam.horbach.ticketService.dto.EventDTO;
 import by.sam.horbach.ticketService.entities.Comment;
-import by.sam.horbach.ticketService.entities.Event;
+import by.sam.horbach.ticketService.services.EventService;
 
 public class IndexFacade {
-	
-	public List<Event> getEvents() {
-		return null;
+
+	TwoWayConverter eventListConverter;
+	EventService eventService;
+
+	@SuppressWarnings("unchecked")
+	public List<EventDTO> getUpcomingEvents() {
+		List<EventDTO> eventsDTO = null;
+		
+		try {
+			eventsDTO = (List<EventDTO>) eventListConverter.convertSourceToTargetClass(eventService.getUpcomingEvents(), List.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return eventsDTO;
 	}
-	
-	
+
 	public List<Comment> getComments() {
 		return null;
 	}
