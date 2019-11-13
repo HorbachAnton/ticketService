80ce3a849451afa1847f66278f1404fde939e962
Horbach Anton <a.horbach@sam-solutions.com>
2019-11-13 14:05:42 +0300

    Changed the  index controller and added welcome controller.

--- src/main/java/by/sam/horbach/ticketService/controllers/IndexPageController.java
+++ src/main/java/by/sam/horbach/ticketService/controllers/IndexPageController.java
@@ -5,17 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import by.sam.horbach.ticketService.facades.IndexFacade;
-
 @Controller
 public class IndexPageController {
 	
-	IndexFacade indexFacade;
-	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String getPage(Model model) {
-		model.addAttribute("upcomingEvents", indexFacade.getUpcomingEvents());
-		return "";
+		return "index";
 	}
 
 }
---
+++ src/main/java/by/sam/horbach/ticketService/controllers/WelcomePageController.java
@@ -0,0 +1,23 @@
+package by.sam.horbach.ticketService.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import by.sam.horbach.ticketService.facades.WelcomeFacade;
+
+@Controller
+public class WelcomePageController {
+	
+	@Autowired
+	WelcomeFacade welcomeFacadeFacade;
+	
+	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	public String getPage(Model model) {
+		model.addAttribute("upcomingEvents", welcomeFacadeFacade.getUpcomingEvents());
+		return "welcome";
+	}
+
+}
