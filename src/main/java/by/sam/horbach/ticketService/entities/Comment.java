0d25d59097914b547d95d1093374ac1623589123
Horbach Anton <a.horbach@sam-solutions.com>
2019-11-14 12:55:20 +0300

    Removed unused methods.

--- src/main/java/by/sam/horbach/ticketService/entities/Comment.java
+++ src/main/java/by/sam/horbach/ticketService/entities/Comment.java
@@ -49,44 +49,4 @@ public class Comment {
 	public void setText(String text) {
 		this.text = text;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + idEvent;
-		result = prime * result + idUser;
-		result = prime * result + ((text == null) ? 0 : text.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comment other = (Comment) obj;
-		if (id != other.id)
-			return false;
-		if (idEvent != other.idEvent)
-			return false;
-		if (idUser != other.idUser)
-			return false;
-		if (text == null) {
-			if (other.text != null)
-				return false;
-		} else if (!text.equals(other.text))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Comment [id=" + id + ", idUser=" + idUser + ", idEvent=" + idEvent + ", text=" + text + "]";
-	}
-
 }
--- src/main/java/by/sam/horbach/ticketService/entities/Ticket.java
+++ src/main/java/by/sam/horbach/ticketService/entities/Ticket.java
@@ -40,37 +40,4 @@ public class Ticket {
 		this.idUser = idUser;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + idEvent;
-		result = prime * result + idUser;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Ticket other = (Ticket) obj;
-		if (id != other.id)
-			return false;
-		if (idEvent != other.idEvent)
-			return false;
-		if (idUser != other.idUser)
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Ticket [id=" + id + ", idEvent=" + idEvent + ", idUser=" + idUser + "]";
-	}
-
 }
