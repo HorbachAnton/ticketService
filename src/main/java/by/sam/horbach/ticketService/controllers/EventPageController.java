92d8bbdde82538a2f3b5b947912f1d0d615a75ef
Horbach Anton <a.horbach@sam-solutions.com>
2019-12-03 18:21:02 +0300

    Added validator for buing tickets.

--- src/main/java/by/sam/horbach/ticketService/controllers/EventPageController.java
+++ src/main/java/by/sam/horbach/ticketService/controllers/EventPageController.java
@@ -3,6 +3,7 @@ package by.sam.horbach.ticketService.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +12,16 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import by.sam.horbach.ticketService.dto.forms.BuyTicketsDTO;
 import by.sam.horbach.ticketService.facades.EventFacade;
+import by.sam.horbach.ticketService.validators.BuyTicketsValidator;
 
 @Controller
 public class EventPageController {
 
 	@Autowired
 	EventFacade eventFacade;
+
+	@Autowired
+	BuyTicketsValidator buyTicketsValidator;
 
 	@RequestMapping(value = "/event_page", method = RequestMethod.GET)
 	public String getPage(@ModelAttribute("eventId") Integer eventId, Model model) {
@@ -26,9 +31,22 @@ public class EventPageController {
 	}
 
 	@RequestMapping(value = "/buy_tickets", method = RequestMethod.POST)
-	public ModelAndView buyTickets(@ModelAttribute("buyTicketsDTO") BuyTicketsDTO buyTicketsDTO) {
+	public ModelAndView buyTickets(@ModelAttribute("buyTicketsDTO") BuyTicketsDTO buyTicketsDTO, BindingResult result) {
+		buyTicketsValidator.validate(buyTicketsDTO, result);
+
+		if (result.hasErrors()) {
+			redirectToEventPage(buyTicketsDTO, result);
+		}
+
 		eventFacade.buyTickets(buyTicketsDTO);
-		return new ModelAndView(new RedirectView("/ticket-service/event_page")).addObject("eventId", buyTicketsDTO.getIdEvent());
+		return new ModelAndView(new RedirectView("/ticket-service/event_page")).addObject("eventId",
+				buyTicketsDTO.getIdEvent());
+	}
+
+	private ModelAndView redirectToEventPage(@ModelAttribute("buyTicketsDTO") BuyTicketsDTO buyTicketsDTO,
+			BindingResult result) {
+		return new ModelAndView(new RedirectView("/ticket-service/event_page")).addObject("eventId",
+				buyTicketsDTO.getIdEvent());
 	}
 
 }
---
+++ src/main/java/by/sam/horbach/ticketService/validators/BuyTicketsValidator.java
@@ -0,0 +1,24 @@
+package by.sam.horbach.ticketService.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import by.sam.horbach.ticketService.dto.forms.BuyTicketsDTO;
+
+public class BuyTicketsValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return BuyTicketsDTO.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		BuyTicketsDTO buyTicketsDTO = (BuyTicketsDTO) target;
+		
+		if(buyTicketsDTO.getQuantity() == 0) {
+			errors.rejectValue("quantity", "errors.null_quantity", "errors.null_quantity.message");
+		}
+	}
+
+}
